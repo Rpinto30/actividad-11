a9db82c188e2325161f1127cba1b8e42de0d983e
Rpinto30 <rodrigopinto3007@gmail.com>
2025-07-31 08:40:25 -0600

    Importación de sistema propio para ingresar cantidad de vhiculos que desea el usuario

--- actividad_11.py
+++ actividad_11.py
@@ -1,1 +1,14 @@
-print("Hola")
+error_mesagge = '-'*50+'\n'+"✖"*5+"   Lo siento, intentelo nuevamente   "+"✖"*5
+
+def input_integer(message): #INGRESAR UN ENTERO Y VERIFICAR QUE SU ENTRADA SEA VALIDA
+    while True:
+        try:
+            value = int(input(message))
+            break
+        except ValueError: print(error_mesagge)
+    return value
+
+while True:
+    n = input_integer("▶  Ingresa la cantidad que deseas agreagar: ")
+    if n > 0: break
+    else: print(error_mesagge)
